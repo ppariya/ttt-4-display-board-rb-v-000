b932bed53f2ab014390f48cfcb315451dc31db7c
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-11-06 09:39:59 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  row_1 = " " + board[0] + " | " + board[1] + " | " + board[2]
+  row_2 = "-----------"
+  row_3 = " " + board[3] + " | " + board[4] + " | " + board[5]
+  row_4 = "-----------"
+  row_5 = " " + board[6] + " | " + board[7] + " | " + board[8]
 end
